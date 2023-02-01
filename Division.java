fc040f93bb5d0f1443a558e022565a3824fa70b4
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2023-02-01 11:53:58 +0530

    import java.util.Scanner; class first {     public static void main(String args[])     {         int p,q;         Scanner sc=new Scanner(System.in);         p=sc.nextInt();         q=sc.nextInt();         System.out.println(p/q);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class first
+{
+    public static void main(String args[])
+    {
+        int p,q;
+        Scanner sc=new Scanner(System.in);
+        p=sc.nextInt();
+        q=sc.nextInt();
+        System.out.println(p/q);
+    }
+}
