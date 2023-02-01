7ac674f3497cafdd0291eb7c7c07e1f4a022ef19
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2023-02-01 12:20:22 +0530

    import java.util.Scanner; class first {     public static void main(String args[])     {         int t,s,b;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         s=sc.nextInt();         b=sc.nextInt();         System.out.println(2*t*s*b*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class first
+{
+    public static void main(String args[])
+    {
+        int t,s,b;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        s=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(2*t*s*b*512);
+    }
+}
